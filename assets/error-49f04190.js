02a8cc0e69e075a4fdca800814a392aa90b901ca
lisonge <lisonge@users.noreply.github.com>
2023-07-07 17:13:24 +0000

    deploy: 0ba732f16c81e2054425d5ab1c264fb945304cd3

--- assets/error-49f04190.js
+++
@@ -1,2 +0,0 @@
-import{P as p}from"./storage-bd0a0faa.js";const g=(e,r,o)=>{try{return e()}catch(t){if(typeof r=="string")p.error(r);else{const s=(r==null?void 0:r(t))??t;typeof s=="string"?p.error(s):s instanceof Error&&p.error(s.message)}throw t}finally{o==null||o()}},i=e=>(...r)=>{try{e(...r)}catch(o){console.error(o)}};export{g as a,i as e};
-//# sourceMappingURL=error-49f04190.js.map
