d693ce0a9fb45ef7406d4f0cdc2d7cffd55db223
Ling0402 <cjy081219@Gmail.com>
2026-06-15 00:13:21 +0800

    feat: extract batch actions for HomePage & DevicePage

---
+++ src/components/BatchActionsBar.vue
@@ -0,0 +1,56 @@
+<script setup lang="tsx">
+defineProps<{
+  checkedCount: number;
+  batchDelete: { loading: boolean; invoke: () => void };
+  batchDownloadImage: { loading: boolean; invoke: () => void };
+  batchDownloadZip: { loading: boolean; invoke: () => void };
+  batchShareImageUrl: { loading: boolean; invoke: () => void };
+  batchShareZipUrl: { loading: boolean; invoke: () => void };
+}>();
+</script>
+<template>
+  <template v-if="checkedCount">
+    <NPopover>
+      <template #trigger>
+        <NButton> 批量下载 </NButton>
+      </template>
+      <NSpace vertical>
+        <NButton
+          :loading="batchDownloadZip.loading"
+          @click="batchDownloadZip.invoke"
+        >
+          批量下载-快照
+        </NButton>
+        <NButton
+          :loading="batchDownloadImage.loading"
+          @click="batchDownloadImage.invoke"
+        >
+          批量下载-图片
+        </NButton>
+      </NSpace>
+    </NPopover>
+    <NPopover>
+      <template #trigger>
+        <NButton> 批量分享 </NButton>
+      </template>
+      <NSpace vertical>
+        <NButton
+          :loading="batchShareZipUrl.loading"
+          @click="batchShareZipUrl.invoke"
+        >
+          批量生成链接-快照
+        </NButton>
+        <NButton
+          :loading="batchShareImageUrl.loading"
+          @click="batchShareImageUrl.invoke"
+        >
+          批量生成链接-图片
+        </NButton>
+      </NSpace>
+    </NPopover>
+    <NButton @click="batchDelete.invoke"> 批量删除 </NButton>
+    <div h-full flex flex-items-center>
+      {{ `已选中 ${checkedCount} 个快照` }}
+    </div>
+  </template>
+</template>
---
+++ src/composables/useBatchActions.ts
@@ -0,0 +1,85 @@
+import { showTextDLg, waitShareAgree } from '@/utils/dialog';
+import { dialog } from '@/utils/discrete';
+import {
+  batchCreateImageId,
+  batchCreateZipUrl,
+  batchImageDownloadZip,
+  batchZipDownloadZip,
+} from '@/utils/export';
+import { snapshotStorage } from '@/utils/snapshot';
+import { useTask } from '@/utils/task';
+import { getImagUrl } from '@/utils/url';
+
+export const useBatchActions = (
+  checkedRowKeys: Ref<number[]>,
+  options: {
+    onAfterDelete?: () => void;
+    beforeDeleteItem?: (id: number) => Promise<unknown>;
+  } = {},
+) => {
+  const checkedSnapshots = () => {
+    return Promise.all(
+      checkedRowKeys.value.map(
+        (id) => snapshotStorage.getItem(id) as Promise<Snapshot>,
+      ),
+    );
+  };
+
+  const batchDelete = useTask(async () => {
+    await new Promise((res, rej) => {
+      dialog.warning({
+        title: `删除`,
+        content: `是否批量删除 ${checkedRowKeys.value.length} 个快照`,
+        negativeText: `取消`,
+        positiveText: `确认`,
+        onClose: rej,
+        onEsc: rej,
+        onMaskClick: rej,
+        onNegativeClick: rej,
+        onPositiveClick: res,
+      });
+    });
+    if (options.beforeDeleteItem) {
+      await Promise.all(
+        checkedRowKeys.value.map((k) => options.beforeDeleteItem!(k)),
+      );
+    }
+    await Promise.all(
+      checkedRowKeys.value.map((k) => snapshotStorage.removeItem(k)),
+    );
+    options.onAfterDelete?.();
+  });
+
+  const batchDownloadImage = useTask(async () => {
+    await batchImageDownloadZip(await checkedSnapshots());
+  });
+
+  const batchDownloadZip = useTask(async () => {
+    await batchZipDownloadZip(await checkedSnapshots());
+  });
+
+  const batchShareImageUrl = useTask(async () => {
+    await waitShareAgree();
+    const imageIds = await batchCreateImageId(await checkedSnapshots());
+    showTextDLg({
+      content: imageIds.map((s) => getImagUrl(s)).join(`\n`) + `\n`,
+    });
+  });
+
+  const batchShareZipUrl = useTask(async () => {
+    await waitShareAgree();
+    const zipUrls = await batchCreateZipUrl(await checkedSnapshots());
+    showTextDLg({
+      content:
+        zipUrls.map((s) => location.origin + '/i/' + s).join(`\n`) + `\n`,
+    });
+  });
+
+  return {
+    batchDelete,
+    batchDownloadImage,
+    batchDownloadZip,
+    batchShareImageUrl,
+    batchShareZipUrl,
+  };
+};
